985b14b5c9ad48ca45e6f3d05936f338b27665aa
Aaron Bean <arbean1978@gmail.com>
2022-08-04 11:03:40 -0600

    more

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,6 +6,7 @@ import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { useRouter } from 'next/router';
+import  abLogo from '../public/assets/AB2.jpeg'
 
 const Navbar = () => {
       const [nav, setNav] = useState(false);
@@ -56,7 +57,7 @@ const Navbar = () => {
           <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                 <Link href='/'>
                   <Image
-                         src='../public/assets/AB2.jpeg' 
+                        src={abLogo}
                         alt='/' 
                         width='75' 
                         height='75' 
